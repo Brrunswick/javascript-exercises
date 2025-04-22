347e16b95f0246d79c155281ad597479ddc9be2e
Brrunswick <122462882+Brrunswick@users.noreply.github.com>
2025-04-22 17:06:54 +0100

    wrote function to remove arguments from array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...nums) {
+    // return array.slice(0, num-1).concat(array.slice(num));
+    newArray = [];
+    array.forEach((item) => {
+        if (!nums.includes(item)) {
+            newArray.push(item);
+        }
+    });
+    return newArray;
 };
-
 // Do not edit below this line
 module.exports = removeFromArray;
