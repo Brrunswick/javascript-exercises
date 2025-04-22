f58eb9b1a260d362bea38c68d3f61349a5113153
Brrunswick <122462882+Brrunswick@users.noreply.github.com>
2025-04-22 17:19:37 +0100

    wrote function in js file and removed .skip in spec file

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = function(numOne, numTwo) {
+    let sum = 0;
 
+    //first line checks to confirm that both numbers given are positive integers
+    if (numOne < 1 || numTwo < 1 || !Number.isInteger(numOne) || !Number.isInteger(numTwo)) {
+        return "ERROR"
+    }
+    else if (numOne < numTwo) {
+        for (let i = numOne; i <= numTwo; i++) {
+            sum += i;
+        }
+        return sum;
+    } else if (numOne > numTwo) {
+        for (let i = numTwo; i <= numOne; i++) {
+            sum += i;
+        }
+        return sum;
+    }
+    
 };
 
 // Do not edit below this line
