cbe0609826a40d61a837447a8213727f19758067
Brrunswick <122462882+Brrunswick@users.noreply.github.com>
2025-04-28 23:54:33 +0100

    wrte palindromes function and removed .skip from spec file

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,22 @@
-const palindromes = function () {
-
+/* const palindromes = function (string) {
+    const alphanumerical = "abcdefghijklmnopqrstuvwxyz0123456789"
+    const newStr = string.toLowerCase().split("").filter((character => alphanumerical.includes(character))).join("");
+    const reverseStr = newStr.split("").reverse().join("");
+    
+    reverseStr === newStr;
 };
+
+*/
+
+const palindromes = function (str) {
+
+    const alphanumerical = "abcdefghijklmnopqrstuvwxyz0123456789"
+    const forward = str.toLowerCase().split('').filter((character => alphanumerical.includes(character))).join('');
+    const reverse = forward.split('').reverse().join('');
+
+    return reverse === forward;
+};
+
 
 // Do not edit below this line
 module.exports = palindromes;
