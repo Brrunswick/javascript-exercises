e3bb66fa78faca66365733d7cd129bbbcc9183c7
Brrunswick <122462882+Brrunswick@users.noreply.github.com>
2025-04-29 11:59:04 +0100

    finished fibonacci function and removed .skip from spec file

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,17 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    const fibArray = [0, 1];
+    let number = parseInt(num);
+    
+    if (number < 0) { 
+        return "OOPS"
+    }
+    
+    for (let i = 2; i <= num + 1; i ++) {
+        fibArray[i] = fibArray[i-2] + fibArray[i-1];
+    }
+    return fibArray[num];
 };
 
 // Do not edit below this line
 module.exports = fibonacci;
+
