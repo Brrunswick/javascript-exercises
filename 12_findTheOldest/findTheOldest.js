14d715808a7b0a46755cdfb631d7b4276595b862
Brrunswick <122462882+Brrunswick@users.noreply.github.com>
2025-04-29 16:17:39 +0100

    wrote a function that finds the oldest person from an array - to do this wrote an additional function it would call to calculate ages

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,31 @@
-const findTheOldest = function() {
+let oldest = 0
+const year = new Date();
 
+const findTheOldest = function(people) {
+    for (i = 0; i < people.length; i ++) {
+        if (people[i].yearOfDeath === undefined) {
+            currentAge = year.getFullYear() - people[i].yearOfBirth;
+        } else {
+            currentAge = people[i].yearOfDeath - people[i].yearOfBirth;
+        }
+        if (currentAge > oldest) {
+            oldest = currentAge;
+        }
+    }
+    return getResult(people);
 };
 
+function getResult(people) {
+    for (i = 0; i < people.length; i ++) {
+        if (people[i].yearOfDeath === undefined) {
+            currentAge = year.getFullYear() - people[i].yearOfBirth;
+        } else {
+            currentAge = people[i].yearOfDeath - people[i].yearOfBirth;
+        }
+        if (currentAge === oldest) {
+            return people[i];
+        }
+    }
+}
 // Do not edit below this line
 module.exports = findTheOldest;
